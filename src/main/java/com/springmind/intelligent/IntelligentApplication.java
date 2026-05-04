1915e789ab28a72c61b6433b61e8ed13adc25ca2
Rahul <mouryarahul543@gmail.com>
2026-05-04 19:53:35 +0530

    initial

---
+++ src/main/java/com/springmind/intelligent/IntelligentApplication.java
@@ -0,0 +1,13 @@
+package com.springmind.intelligent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntelligentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntelligentApplication.class, args);
+	}
+
+}
