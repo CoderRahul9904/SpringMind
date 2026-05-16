cd8c421310c1c3e8b4e11ab193ba40b615d57d4b
Rahul <mouryarahul543@gmail.com>
2026-05-16 23:37:34 +0530

    Adds a test to store data in the vector database.

--- src/test/java/com/springmind/intelligent/IntelligentApplicationTests.java
+++ src/test/java/com/springmind/intelligent/IntelligentApplicationTests.java
@@ -1,13 +1,27 @@
 package com.springmind.intelligent;
 
+import com.springmind.intelligent.helper.Helper;
+import com.springmind.intelligent.service.ChatClientService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
 
 @SpringBootTest
 class IntelligentApplicationTests {
 
+	@Autowired
+	public ChatClientService chatClientService;
+
 	@Test
-	void contextLoads() {
+	public void checkDataStoreInVectorDB(){
+		System.out.println("Putting Data in Vector DB - MariaDB");
+		this.chatClientService.storeDocumentedData(Helper.getData());
+		System.out.println("Data is Stored in MariaDB");
 	}
+
+
+
 
 }
